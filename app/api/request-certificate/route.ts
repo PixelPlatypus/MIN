64516ad037a8c6da23607d6f506f6b329a6276e9
PixelPlatypus <manishpoudel2060@gmail.com>
2025-07-30 14:58:23 +0545

    fix: /request-certificate email problem

--- app/api/request-certificate/route.ts
+++ app/api/request-certificate/route.ts
@@ -17,8 +17,8 @@ export async function POST(request: Request) {
     // Create a Nodemailer transporter
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
--- app/api/request-partnership/route.ts
+++ app/api/request-partnership/route.ts
@@ -11,7 +11,6 @@ export async function POST(request: Request) {
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
       secure: false, // Use STARTTLS
-      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
